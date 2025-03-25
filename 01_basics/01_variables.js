ded9d503b5082bdd7367d0dca393b9af87a609cd
Delonix-0 <96078033+Delonix-0@users.noreply.github.com>
2025-03-25 15:12:48 +0000

    data and var

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId= 1234
+let accountEmail="Deepali@gmail.com"
+var accountPassword="Deepali21"
+accountCity="Jaipur"
+
+//accountId=2 not allowed
+accountEmail="De@gmail"
+accountPassword="234"
+accountCity="Ayodhya"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity])
