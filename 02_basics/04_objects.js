6c96621c19f61e9d65982b7378c81557e4e7ccd2
Delonix-0 <96078033+Delonix-0@users.noreply.github.com>
2025-03-26 07:28:44 +0000

    obj

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -5,3 +5,20 @@ tinderUser.name="Deep"
 tinderUser.isLoggedIn=false
 
 console.log(tinderUser);
+
+const obj1={1: "a", 2: "b"}
+const obj2={3: "a", 4: "b"}
+const obj4={5: "a", 6: "b"}
+//const obj3 = Object.assign({}, obj1,obj2,obj4)
+
+
+//console.log(obj3);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+
+
